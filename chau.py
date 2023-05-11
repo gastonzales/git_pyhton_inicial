cc434616a8155b702452edb348ac66f0348e7a67
gastonzales <125465281+gastonzales@users.noreply.github.com>
2023-05-11 17:16:30 -0300

    agregue el archivo nuevo

---
+++ chau.py
@@ -0,0 +1,6 @@
+#git init           iniciA rep
+#git status -s      muestra estado de archivos
+#git add .          lleva archivos al area temporal   o git add nombre lleva el archivo nombre solo
+#git commit -m "comentario"     lleva archivo al repo local con su comentario
+#git push -u origin main          subimos a nub
+
